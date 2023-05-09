005062b41719d6446c7dbe0beee5272a6a939153
Vladimir Shytsko <shytsko.v@gmail.com>
2023-05-09 21:30:23 +0300

    update

--- re/re_ex1.py
+++ re/re_ex1.py
@@ -1,12 +1,12 @@
 import re
 
-text = "fghjserg  sg seh erg vov_an@tut.by tyjktr hrgrtty ghrhtyjt rthtjkktrjeh rtgrgtkjyukyuhde +375295170293" \
-       " drthtrj ert srthrsth sdgsd shy56tsko.v@gm.ail.com"
+text = "+375295170293 fghjserg  sg seh erg vov_an@tut.by tyjktr hrgrtty ghrhtyjt rthtjkktrjeh rtgrgtkjyukyuhde " \
+       "+375295170293 drthtrj ert srthrsth sdgsd shy56tsko.v@gm.ail.com"
 
-tel_pattern = r"\s\+(\d{10,12})\s"
+tel_pattern = r"\B\+\d{10,12}\b"
 tel = re.findall(tel_pattern, text)
 print(tel)
 
-e_mail_pattern = r"\s([a-zA-Z][\w._]+@[\w._]+[.][\w]+)$|/s"
+e_mail_pattern = r"\b([a-zA-Z][\w._]+@[\w._]+[.][\w]+)\b"
 email = re.findall(e_mail_pattern, text)
 print(email)
